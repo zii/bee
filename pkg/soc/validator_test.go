6eb3645e34fe185ed4990a6e66e0422e76d1e165
acud <12988138+acud@users.noreply.github.com>
2021-02-05 11:41:28 +0100

    soc: wip deterministic soc test

--- pkg/soc/validator_test.go
+++ pkg/soc/validator_test.go
@@ -6,6 +6,7 @@ package soc_test
 
 import (
 	"encoding/binary"
+	"encoding/hex"
 	"testing"
 
 	"github.com/ethersphere/bee/pkg/crypto"
@@ -22,6 +23,56 @@ func TestValidator(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+	signer := crypto.NewDefaultSigner(privKey)
+
+	bmtHashOfFoo := "2387e8e7d8a48c2a9339c97c1dc3461a9a7aa07e994c5cb8b38fd7c1b3e6ea48"
+	address := swarm.MustParseHexAddress(bmtHashOfFoo)
+	foo := "foo"
+	fooLength := len(foo)
+	fooBytes := make([]byte, 8+fooLength)
+	binary.LittleEndian.PutUint64(fooBytes, uint64(fooLength))
+	copy(fooBytes[8:], foo)
+	ch := swarm.NewChunk(address, fooBytes)
+
+	sch, err := soc.NewChunk(id, ch, signer)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// check valid chunk
+	if !soc.Valid(sch) {
+		t.Fatal("valid chunk evaluates to invalid")
+	}
+
+	// check invalid data
+	sch.Data()[0] = 0x01
+	if soc.Valid(sch) {
+		t.Fatal("chunk with invalid data evaluates to valid")
+	}
+
+	// check invalid address
+	sch.Data()[0] = 0x00
+	wrongAddressBytes := sch.Address().Bytes()
+	wrongAddressBytes[0] = 255 - wrongAddressBytes[0]
+	wrongAddress := swarm.NewAddress(wrongAddressBytes)
+	sch = swarm.NewChunk(wrongAddress, sch.Data())
+	if soc.Valid(sch) {
+		t.Fatal("chunk with invalid address evaluates to valid")
+	}
+}
+
+func TestValidatorDeterministic(t *testing.T) {
+	id := make([]byte, soc.IdSize)
+	data, err := hex.DecodeString("634fb5a872396d9693e5c9f9d7233cfa93f395c093371017ff44aa9ae6564cdd")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	privKey, err := crypto.DecodeSecp256k1PrivateKey(data)
+	if err != nil {
+		t.Fatal(err)
+	}
+
 	signer := crypto.NewDefaultSigner(privKey)
 
 	bmtHashOfFoo := "2387e8e7d8a48c2a9339c97c1dc3461a9a7aa07e994c5cb8b38fd7c1b3e6ea48"
